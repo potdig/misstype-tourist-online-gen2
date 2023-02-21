92ab771b6973dc4c11913682b70c37993aa006bf
Shouhei Hayakawa <s-hayakawa@esm.co.jp>
2023-02-21 23:16:42 +0900

    set up NodeCG environment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  base: '/bundles/misstype-tourist-online-gen2/',
   server: {
     host: true,
   },
