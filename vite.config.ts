16faafffa5dee3df15b95ebdaa3dccf24f6037e9
potdig <potadig.ashnerd.zino@gmail.com>
2024-12-26 23:10:15 +0900

    adjust Docker environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'package-nodecg.json',
+          dest: '.',
+          rename: 'package.json',
+        },
+      ],
+    }),
+  ],
   base: '/bundles/misstype-tourist-online-gen2/',
   server: {
     host: true,
